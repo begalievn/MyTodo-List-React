4e49100da43aeca6c1a29216dd3f9adde5b5987d
begalievn <begaliev.info@gmail.com>
2022-03-29 00:02:58 +0600

    removed select part

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-const Form = ({ inputText, setInputText, todos, setTodos, setData }) => {
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
   const inputTextHandler = (e) => {
     setInputText(e.target.value);
   };
 
   const submitTodoHandler = (e) => {
     e.preventDefault();
-    setTodos([
-      ...todos,
-      { text: inputText, completed: false, id: Math.random() * 1000 },
-    ]);
-    setInputText("");
+    if (inputText !== "") {
+      setTodos([
+        ...todos,
+        { text: inputText, completed: false, id: Math.random() * 1000 },
+      ]);
+      setInputText("");
+    }
   };
 
   return (
@@ -25,13 +27,6 @@ const Form = ({ inputText, setInputText, todos, setTodos, setData }) => {
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
-      <div className="select">
-        <select name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
     </form>
   );
 };
